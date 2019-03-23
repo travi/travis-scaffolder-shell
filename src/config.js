a55cac9e84e76eaa6178fe9ed484a2e4f9421895
Matt Travi <programmer@travi.org>
2019-03-23 16:27:17 -0500

    feat(bpkg): instructed travis to install the package manager

--- src/config.js
+++ src/config.js
@@ -3,6 +3,12 @@ import writeYaml from '../third-party-wrappers/write-yaml';
 export default function ({projectRoot}) {
   return writeYaml(
     `${projectRoot}/.travis.yml`,
-    {language: 'generic', notifications: {email: false}, install: 'bpkg getdeps', script: 'make test'}
+    {
+      language: 'generic',
+      notifications: {email: false},
+      before_install: 'curl -Lo- "https://raw.githubusercontent.com/bpkg/bpkg/master/setup.sh" | bash',
+      install: 'bpkg getdeps',
+      script: 'make test'
+    }
   );
 }
--- test/unit/config-test.js
+++ test/unit/config-test.js
@@ -27,6 +27,7 @@ suite('config file generation', () => {
       {
         language: 'generic',
         notifications: {email: false},
+        before_install: 'curl -Lo- "https://raw.githubusercontent.com/bpkg/bpkg/master/setup.sh" | bash',
         install: 'bpkg getdeps',
         script: 'make test'
       }
